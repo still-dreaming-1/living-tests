292ac90389127ed8546032a1c1f2e1d278e01f05
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-08-22 17:47:00 -0600

    Refactored: Deleted livingTests.js:
    It was not being used in any way.

--- livingTests.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-const repl = require('repl')
-let Test= require('./Test.js')
-let test= Test()
-let replServer= repl.start()
-replServer.beforeEach= test.beforeEach
-replServer.spec= test.spec
