d65c6cbe74cf8a9c0e8006a296ca40e226f487a3
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-08-22 14:37:37 -0600

    Showing checkmarks next to passing assertions instead of the word pass

--- assert.js
+++ assert.js
@@ -5,7 +5,7 @@ let assert= require('assert')
 let Assert= (state={}) => {
 	state.equal= (expected, actual) => {
 		assert.equal(expected, actual)
-		return 'pass'
+		return '✔' // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
 	}
 	return state
 }
