a6fedcca93c78da236178748288fd078623ebefe
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-08-22 19:16:09 -0600

    Started making livingTests.js the main require file that gives you access to the rest of the library

--- livingTests.js
+++ livingTests.js
@@ -1,7 +1,2 @@
-"use strict"
-
-// This file is not finished yet. You use it by running node from the terminal and passing this file to it. It will search the directory you
-// started node from for a directory called 'tests'. It will then recursively run all the tests from all the .js.
-
-let testsDir= require('process').cwd() + '/tests'
-let fs= require('fs')
+let livingTests= (state={}) => {
+}
