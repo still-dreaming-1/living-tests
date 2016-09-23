59f4535deea0a180527be9444360593c6b0faf42
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-09-23 08:52:51 -0600

    Started making living-tests a terminal tool:
    It does not work yet, but I want typing living-tests from the
    terminal to run all tests in your project using node without using
    it as a REPL and without using codi.

--- run.js
+++ run.js
@@ -1,7 +1,13 @@
+// #!/usr/bin/env node
 "use strict"
 
-// This file is not finished yet. You use it by running node from the terminal and passing this file to it. It will search the directory you
+// This file is not finished yet. You use it typing living-tests into the terminal. It will search the directory you
 // started node from for a directory called 'tests'. It will then recursively run all the tests from all the .js.
 
-let testsDir= require('process').cwd() + '/tests'
 let fs= require('fs')
+let testsDirPath= require('process').cwd() + '/tests'
+let elhin= require('elhin')()
+const testsDir= elhin.Dir(testsDirPath)
+console.log('expected tests dir: ' + testsDir.path)
+testsDir.get_files_with_extension_recursive('js')
+
