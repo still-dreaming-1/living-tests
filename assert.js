21048e5baaae34e462cccfa2a9d7f231525287a9
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-09-23 08:50:39 -0600

    Added support for assert.deepEqual()

--- assert.js
+++ assert.js
@@ -3,9 +3,14 @@
 let originalAssert= require('assert')
 
 let assert= (state={}) => {
+	const passString= '✔' // returning this after an assertion does not fail is needed because the REPL will output whatever the assertion returns.
 	state.equal= (expected, actual) => {
 		originalAssert.equal(expected, actual)
-		return '✔' // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
+		return passString 
+	}
+	state.deepEqual= (expected, actual) => {
+		originalAssert.deepEqual(expected, actual)
+		return passString // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
 	}
 	return state
 }
