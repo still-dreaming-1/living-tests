0c423cdc2cc5992ea0348c43b789adef8f835e0e
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-09 20:49:24 -0600

    the test is more illustrative of how I recommend creating tests now

--- tests/Test_test.js
+++ tests/Test_test.js
@@ -3,9 +3,11 @@
 // you will need this require in your own tests, but yours can be simpler. You can just use "let assert= require('living-tests')".
 const assert= require('../assert') 
 
+// create the main variables used by the tests and setup by the setup function here
+let five = null
 // create a setup function that you will call before tests that need to be setup
-const setup= () => {
-	this.five= 5 // For variables that need to be setup add them to "this". This prevents duplication from having to create variables above this function and then setting them inside
+const setup = () => {
+	five = 5
 }
 
 // manually call setup() before tests that need to be setup. It is up to you, to write setup() in such a way that it will ensure all your tests run the same.
@@ -13,9 +15,9 @@ const setup= () => {
 
 // test setup
 setup()
-assert.equal(5, this.five)
-this.five= 10
+assert.equal(5, five)
+five = 10
 
-// even though the previous test set this.five to 10, it should be back to 5 here
+// even though the previous test set five to 10, it should be back to 5 here, unless you forget to call the setup function
 setup()
-assert.equal(5, this.five)
+assert.equal(5, five)
