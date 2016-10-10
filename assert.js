a5b0e76906699c26b4f86c41888b396ee0dfa753
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-09 20:12:50 -0600

    refactored internal code style: underscores, not camel casing

--- assert.js
+++ assert.js
@@ -1,15 +1,15 @@
 "use strict"
 
-let originalAssert = require('assert')
+let original_assert = require('assert')
 
 let assert = (state = {}) => {
 	const passString = '✔' // returning this after an assertion does not fail is needed because the REPL will output whatever the assertion returns.
 	state.equal = (expected, actual) => {
-		originalAssert.equal(expected, actual)
+		original_assert.equal(expected, actual)
 		return passString
 	}
 	state.deepEqual = (expected, actual) => {
-		originalAssert.deepEqual(expected, actual)
+		original_assert.deepEqual(expected, actual)
 		return passString // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
 	}
 	return state
--- run.js
+++ run.js
@@ -5,8 +5,8 @@
 // started node from for a directory called 'tests'. It will then recursively run all the tests from all the .js.
 
 let fs = require('fs')
-let testsDirPath = require('process').cwd() + '/tests'
+let tests_dir_path = require('process').cwd() + '/tests'
 let elhin = require('elhin')()
-const testsDir = elhin.Dir(testsDirPath)
-console.log('expected tests dir: ' + testsDir.path)
-testsDir.get_files_with_extension_recursive('js')
+const tests_dir = elhin.Dir(tests_dir_path)
+console.log('expected tests dir: ' + tests_dir.path)
+tests_dir.get_files_with_extension_recursive('js')
