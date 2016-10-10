a2bfa8118c55f555c50d8b3ef1cb15ebb71cebe6
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-09 21:27:25 -0600

    Fixed bug in assert: swapped actual and expected parameters

--- assert.js
+++ assert.js
@@ -6,14 +6,14 @@ const assert = (object = {}) => {
 	const pass_string = '✔' // returning this after an assertion does not fail is needed because the REPL will output whatever the assertion returns.
 
 	// A strict equality assertion. Testing tools should use strict equality comparison as the default, not the other way around.
-	object.equal = (expected, actual) => {
-		original_assert.strictEqual(expected, actual)
+	object.equal = (actual, expected) => {
+		original_assert.strictEqual(actual, expected)
 		return pass_string
 	}
 
 	// A strict, deep equality assertion. Testing tools should use strict equality comparison as the default, not the other way around.
-	object.deep_equal = (expected, actual) => {
-		original_assert.deepStrictEqual(expected, actual)
+	object.deep_equal = (actual, expected) => {
+		original_assert.deepStrictEqual(actual, expected)
 		return pass_string // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
 	}
 
