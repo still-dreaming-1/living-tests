40935d85b9c6cd2a157f7d971ff9528870240bb7
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-09 20:17:01 -0600

    refactored:
    Using the name object instead of state. This is a more untuitive
    and familiar name. It does a better job of implying this is a type
    of object oriented programming. The name "state" implied that we
    were carefully managing state, which is a different concept.

--- assert.js
+++ assert.js
@@ -2,17 +2,17 @@
 
 let original_assert = require('assert')
 
-let assert = (state = {}) => {
+let assert = (object = {}) => {
 	const pass_string = '✔' // returning this after an assertion does not fail is needed because the REPL will output whatever the assertion returns.
-	state.equal = (expected, actual) => {
+	object.equal = (expected, actual) => {
 		original_assert.equal(expected, actual)
 		return pass_string
 	}
-	state.deepEqual = (expected, actual) => {
+	object.deepEqual = (expected, actual) => {
 		original_assert.deepEqual(expected, actual)
 		return pass_string // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
 	}
-	return state
+	return object
 }
 
 module.exports = assert()
