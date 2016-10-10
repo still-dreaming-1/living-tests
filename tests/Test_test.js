ea3e47b251de4ca4e20fc5fe637dd23855bdb46a
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-09 20:56:11 -0600

    Improved code comments in test

--- tests/Test_test.js
+++ tests/Test_test.js
@@ -1,6 +1,6 @@
 // This is an example of how to write a test for living-tests. I am using living-tests to test itself, or in this case, to test the concept / instructions for writing these tests
 
-// you will need this require in your own tests, but yours can be simpler. You can just use "let assert= require('living-tests')".
+// you will need this require in your own tests, but yours can be simpler. You can just use "const assert = require('living-tests')".
 const assert= require('../assert')
 
 // create the main variables used by the tests and setup by the setup function here
@@ -9,15 +9,17 @@ let five = null
 const setup = () => {
 	five = 5
 }
+// you can create a tear_down() function here if you tests need one. These tests don't need that
 
-// manually call setup() before tests that need to be setup. It is up to you, to write setup() in such a way that it will ensure all your tests run the same.
 // Unlike other testing frameworks, you don't pass a description of what you are testing. For now just use comments for that:
 
-// test setup
+// test setup and assert.equal
 setup()
 assert.equal(5, five)
 five = 10
+// Call the tear_down() function here if you have one. These tests don't have one.
 
 // even though the previous test set five to 10, it should be back to 5 here, unless you forget to call the setup function
 setup()
 assert.equal(5, five)
+// Call the tear_down() function here if you have one. These tests don't have one.
