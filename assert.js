1ea4cedac9821c885d38e4557663b1e304f9dcef
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-09 20:02:38 -0600

    code style refactoring: use single space before and after =

--- assert.js
+++ assert.js
@@ -1,18 +1,18 @@
 "use strict"
 
-let originalAssert= require('assert')
+let originalAssert = require('assert')
 
-let assert= (state={}) => {
-	const passString= '✔' // returning this after an assertion does not fail is needed because the REPL will output whatever the assertion returns.
-	state.equal= (expected, actual) => {
+let assert = (state = {}) => {
+	const passString = '✔' // returning this after an assertion does not fail is needed because the REPL will output whatever the assertion returns.
+	state.equal = (expected, actual) => {
 		originalAssert.equal(expected, actual)
 		return passString
 	}
-	state.deepEqual= (expected, actual) => {
+	state.deepEqual = (expected, actual) => {
 		originalAssert.deepEqual(expected, actual)
 		return passString // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
 	}
 	return state
 }
 
-module.exports= assert()
+module.exports = assert()
--- run.js
+++ run.js
@@ -4,10 +4,9 @@
 // This file is not finished yet. You use it typing living-tests into the terminal. It will search the directory you
 // started node from for a directory called 'tests'. It will then recursively run all the tests from all the .js.
 
-let fs= require('fs')
-let testsDirPath= require('process').cwd() + '/tests'
-let elhin= require('elhin')()
-const testsDir= elhin.Dir(testsDirPath)
+let fs = require('fs')
+let testsDirPath = require('process').cwd() + '/tests'
+let elhin = require('elhin')()
+const testsDir = elhin.Dir(testsDirPath)
 console.log('expected tests dir: ' + testsDir.path)
 testsDir.get_files_with_extension_recursive('js')
-
