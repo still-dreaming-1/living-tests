51a760983571f7850ec366d143da19351149fde2
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-09 20:42:08 -0600

    refactored: using const when possible

--- tests/Test_test.js
+++ tests/Test_test.js
@@ -1,10 +1,10 @@
 // This is an example of how to write a test for living-tests. I am using living-tests to test itself, or in this case, to test the concept / instructions for writing these tests
 
 // you will need this require in your own tests, but yours can be simpler. You can just use "let assert= require('living-tests')".
-let assert= require('../assert') 
+const assert= require('../assert') 
 
 // create a setup function that you will call before tests that need to be setup
-let setup= () => {
+const setup= () => {
 	this.five= 5 // For variables that need to be setup add them to "this". This prevents duplication from having to create variables above this function and then setting them inside
 }
 
