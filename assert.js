826b171d18775af82af373b087ec59d84d62faf4
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-09 20:14:19 -0600

    refactored internal code style: underscores, not camel casing

--- assert.js
+++ assert.js
@@ -3,14 +3,14 @@
 let original_assert = require('assert')
 
 let assert = (state = {}) => {
-	const passString = '✔' // returning this after an assertion does not fail is needed because the REPL will output whatever the assertion returns.
+	const pass_string = '✔' // returning this after an assertion does not fail is needed because the REPL will output whatever the assertion returns.
 	state.equal = (expected, actual) => {
 		original_assert.equal(expected, actual)
-		return passString
+		return pass_string
 	}
 	state.deepEqual = (expected, actual) => {
 		original_assert.deepEqual(expected, actual)
-		return passString // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
+		return pass_string // returning a check mark to indicate the assertion passed. This is needed because the REPL will output whatever this function returns.
 	}
 	return state
 }
