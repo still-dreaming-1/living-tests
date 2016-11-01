b688d9e9bce86981433cc52cb7b1cf76a0ac3852
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-10-31 18:46:20 -0600

    Added tests

---
+++ tests/integration/when_create_directory_and_delete_it.js
@@ -0,0 +1,39 @@
+"use strict"
+
+// when create() a directory
+
+const assert = require('../../assert')
+const Dir = require('../../elhin/File')
+const Living_tests = require('../../Living_tests')
+
+let test_data_dir = null
+const Test_data_dir = () => {
+	return Living_tests().dir.get_contained_dir('test data')
+}
+
+const setup = () => {
+	tear_down_without_assertions_or_exceptions()
+	test_data_dir = Test_data_dir()
+	assert.equal(test_data_dir.exists(), false)
+	test_data_dir.create()
+	assert.equal(test_data_dir.exists(), true)
+	test_data_dir.delete()
+}
+
+const tear_down_without_assertions_or_exceptions = () => {
+	test_data_dir = Test_data_dir()
+	if(test_data_dir.exists())
+		test_data_dir.delete()
+}
+
+const tear_down = () => {
+	test_data_dir = Test_data_dir()
+	if(test_data_dir.exists())
+		test_data_dir.delete()
+	assert.equal(test_data_dir.exists(), false)
+}
+
+// the directory should not exist
+setup()
+assert.equal(test_data_dir.exists(), false)
+tear_down()
