a3537518811c432789bad94137cbd6cc4dcc0440
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-11-04 08:57:36 -0600

    Started using TDD to make the terminal test runner

---
+++ Runner.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const Runner = (object = {}) => {
+	return object
+}
+
+module.exports = Runner
---
+++ tests/Runner_test.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const Runner = require('../Runner')
+
+const runner = Runner()
