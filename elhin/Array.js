7355fde5df59545b6bd022a664cb9f082bcddcaa
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-11-11 08:41:41 -0700

    Fixed Exception message using wrong method name

--- elhin/Array.js
+++ elhin/Array.js
@@ -1,7 +1,7 @@
 'use strict'
 const assert = require('assert')
 
-assert(!Array.prototype.hasOwnProperty('contains_test'), 'Array.contains_test() already exists. See, they told you monkey patching was bad...')
+assert(!Array.prototype.hasOwnProperty('containsTest'), 'Array.containsTest() already exists. See, they told you monkey patching was bad...')
 Array.prototype.containsTest = function(test) {
 	for(let value of this) {
 		if(test(value))
