2329779ca9d43f9742154351993afb096ff070ce
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-11-21 08:00:39 -0700

    More thorough tests

--- tests/elhin/integration/when_create_file_and_append_2_lines.js
+++ tests/elhin/integration/when_create_file_and_append_2_lines.js
@@ -28,14 +28,12 @@ const setup = () => {
 
 const tear_down_without_assertions_or_exceptions = () => {
 	test_data_dir = Test_data_dir()
-	if(test_data_dir.exists())
-		test_data_dir.delete()
+	test_data_dir.delete_if_exists()
 }
 
 const tear_down = () => {
 	test_data_dir = Test_data_dir()
-	if(test_data_dir.exists())
-		test_data_dir.delete()
+	assert.equal(test_data_dir.delete_if_exists(), true)
 	assert.equal(test_data_dir.exists(), false)
 }
 
@@ -93,3 +91,9 @@ assert.equal(lines[0], 'the first line')
 assert.equal(lines[1], 'that other line')
 assert.equal(lines[2], '')
 tear_down()
+
+// delete_if_exists() deletes the dir even though it contains this file
+setup()
+assert.equal(test_data_dir.exists(), true)
+assert.equal(test_data_dir.delete_if_exists(), true)
+assert.equal(test_data_dir.exists(), false)
