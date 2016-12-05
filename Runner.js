3a284ed8b099ef7c2c2a3558a8072e71311ede5f
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2016-12-05 10:18:26 -0700

    Made Runner more testable

--- Runner.js
+++ Runner.js
@@ -2,23 +2,25 @@
 
 const Runner = (object = {}) => {
 	object.living_tests = require('./Living_tests')()
+
 	object.run = () => {
 		const tests = object.living_tests.Tests(object.living_tests.elhin.Dir(process.cwd()))
 
 		const test_file_array = tests.collect_files()
 		const test_result_array = tests.run(test_file_array)
 
+		const con = object.living_tests.elhin.Console()
 		for(let test_result of test_result_array) {
 			if(test_result.overall_result()) {
-				console.log('pass: ' + test_result.test_file.path)
+				con.log('pass: ' + test_result.test_file.path)
 			} else {
-				console.log('fail: ' + test_result.test_file.path)
-				console.log(test_result)
+				con.log('fail: ' + test_result.test_file.path)
+				con.log(test_result)
 				process.exit()
 			}
 		}
-		console.log()
-		console.log(require('./assert').pass_string)
+		con.log()
+		con.log(require('./assert').pass_string)
 	}
 	return object
 }
