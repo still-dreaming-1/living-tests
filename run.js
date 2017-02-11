5c4b5ac795d8deff79b1f1c6b2df36f51e395bb9
still-dreaming-1 <still-dreaming-1@users.noreply.github.com>
2017-02-11 15:38:04 -0700

    Added documenation for running all your tests at once.

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict'
 
-// You use it typing living-tests into the terminal. It will asume the directory you started it from is the your project directory. It looks for a directory called tests and
+// Use it by typing living-tests into the terminal. It will asume the directory you started it from is the your project directory. It looks for a directory called tests and
 // recusrively runs all the .js files inside it as if they were tests.
 const runner = require('./Runner')()
 runner.run()
